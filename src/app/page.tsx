5bcbb2271d5e4eb464b91450f5b3b70c59f96803
Marcos Pedro <king.frontweb@outlook.com>
2025-08-13 17:05:05 -0300

    feat: home frontend done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,29 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
+import { Input } from '@/components/ui/input';
+import { Bookmark, Search } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
+    <main className="flex flex-col items-center justify-center h-full">
+      <div className="w-full flex flex-col gap-20 items-center text-center max-w-[1155px] gap-14">
+        <div>
+          <h2 className="text-[40px] font-bold mb-4">
+            Ache sua vaga <span className="text-[#FE8A00]">ideal</span>{' '}
+            <span className="block">agora mesmo</span>
+          </h2>
+          <p className="text-2xl">
+            Digite o que procura e encontre as melhores oportunidades.
+          </p>
+        </div>
+        <div className="w-full">
+          <div className="flex justify-center w-full">
+            <div className="relative w-full">
+              <Input className="!bg-[#2D3D50] border-[#6C7E94] rounded-lg py-8 px-3 text-4xl" />
+              <div className="bg-[#FE8A00] p-3 absolute right-3 top-1/2 -translate-y-1/2 text-white flex justify-center items-center rounded-md">
+                <Search className="text-xl" />
               </div>
             </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
-        </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main>
         </div>
-
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
       </div>
     </main>
   );
