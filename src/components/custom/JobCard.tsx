016d3e2f20618eaf7749e3a5b5c4b1a7c93aefbf
Marcos Pedro <king.frontweb@outlook.com>
2025-08-15 08:43:38 -0300

    feat: small Jobcard/jobLogo comp

--- src/components/custom/JobCard.tsx
+++ src/components/custom/JobCard.tsx
@@ -2,14 +2,19 @@
 import Image from 'next/image';
 import { LabelJobInfo } from './LabelJobInfo';
 import { LabelTag } from './LabelTag';
-import { Bookmark } from 'lucide-react';
 import { Job } from '@/src/app/api/jobs/route';
 import { limitText } from '@/src/app/helpers/limitText';
-import { toast } from 'react-toastify';
-import { createClient } from '@/src/lib/supabase/client';
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import JobSaveIcon from './JobSaveIcon';
+import JobLogo from './JobLogo';
 
-export default function JobCard({ job }: { job: Job }) {
+export default function JobCard({
+  job,
+  small = false,
+}: {
+  job: Job;
+  small: boolean;
+}) {
   const {
     id,
     title,
@@ -21,67 +26,96 @@ export default function JobCard({ job }: { job: Job }) {
     salary,
     category,
   } = job;
-  const [user, setUser] = useState<any>(null);
-  const supabase = createClient();
+  const appUrl = process.env.NEXT_PUBLIC_APP_URL;
 
-  const saveFavorite = async (e: React.MouseEvent) => {
-    e.stopPropagation();
-    const { data } = await supabase.auth.getUser();
-    setUser(data.user);
+  const route = useRouter();
 
-    if (!data.user) {
-      toast.warning('É necessário fazer o login para salvar esta vaga!');
-      return;
-    }
-
-    console.log('usuario logado');
-    // salvarFavorito(job.id);
-  };
-
-  return (
-    <button
-      type="button"
-      className="bg-[#2D3D50] hover:bg-[#202F40] border-[#6C7E94] border-[1px] rounded-lg w-full transition-all ease-in duration-75 me-[6px] mb-4 p-6 cursor-pointer flex justify-between items-start gap-4"
-      onClick={() => {
-        console.log('Clique no card → abrir detalhes do job');
-      }}
-    >
-      <div className="flex-shrink-0">
-        <Image
-          src={company_logo}
-          alt="Logo da empresa"
-          width={120}
-          height={120}
-          className="min-w-[120px]"
-        />
-      </div>
-      <div className="flex flex-col justify-start items-start gap-3 flex-grow">
-        <h2 className="text-xl font-bold leading-tight text-start">{title}</h2>
-        <div className="flex flex-col items-start gap-1">
-          <div className="flex justify-start items-center gap-2 flex-wrap">
-            <LabelJobInfo type={'company'} text={company_name} />
-            <LabelJobInfo
-              type={'location'}
-              text={candidate_required_location}
-            />
-            {salary && (
-              <LabelJobInfo type={'cash'} text={`${limitText(salary)}`} />
-            )}
-            <LabelJobInfo type={'contract'} text={job_type} />
+  if (small) {
+    return (
+      <button
+        type="button"
+        className="bg-[#2D3D50] hover:bg-[#202F40] border-[#6C7E94] border-[1px] rounded-lg w-full transition-all ease-in duration-75 me-[6px] mb-2 p-6 cursor-pointer flex flex-col justify-between items-start gap-4"
+        onClick={() => {
+          route.push(
+            `${appUrl}/jobs/${id}?title=${title}&company_name=${company_name}&category=${category}`,
+          );
+        }}
+      >
+        <div className="flex justify-between w-full">
+          <div className="flex-shrink-0 me-2">
+            <JobLogo src={company_logo} small={true} />
           </div>
-          <div className="block">
-            <LabelJobInfo type={'category'} text={category} />
+          <div className="flex flex-col justify-start items-start gap-3 flex-grow">
+            <h2 className="text-xl font-bold leading-tight text-start">
+              {title}
+            </h2>
+            <div className="flex flex-col items-start gap-1 text-start">
+              <span>
+                {company_name} · {candidate_required_location}
+              </span>
+            </div>
           </div>
+          <JobSaveIcon
+            job_id={id.toString()}
+            category={category}
+            company_name={company_name}
+            search={title}
+          />
         </div>
         <div className="flex justify-start items-center gap-2 flex-wrap">
           {tags.map((tag, index) => {
-            if (index < 6) return <LabelTag key={tag} text={tag} />;
+            if (index < 4) return <LabelTag key={tag} text={tag} />;
           })}
         </div>
-      </div>
-      <button type="button" onClick={saveFavorite}>
-        <Bookmark width={30} height={30} />
       </button>
-    </button>
-  );
+    );
+  } else {
+    return (
+      <button
+        type="button"
+        className="bg-[#2D3D50] hover:bg-[#202F40] border-[#6C7E94] border-[1px] rounded-lg w-full transition-all ease-in duration-75 me-[6px] mb-4 p-6 cursor-pointer flex justify-between items-start gap-4"
+        onClick={() => {
+          route.push(
+            `${appUrl}/jobs/${id}?title=${title}&company_name=${company_name}&category=${category}`,
+          );
+        }}
+      >
+        <div className="flex-shrink-0">
+          <JobLogo src={company_logo} />
+        </div>
+        <div className="flex flex-col justify-start items-start gap-3 flex-grow">
+          <h2 className="text-xl font-bold leading-tight text-start">
+            {title}
+          </h2>
+          <div className="flex flex-col items-start gap-1">
+            <div className="flex justify-start items-center gap-2 flex-wrap">
+              <LabelJobInfo type={'company'} text={company_name} />
+              <LabelJobInfo
+                type={'location'}
+                text={candidate_required_location}
+              />
+              {salary && (
+                <LabelJobInfo type={'cash'} text={`${limitText(salary)}`} />
+              )}
+              <LabelJobInfo type={'contract'} text={job_type} />
+            </div>
+            <div className="block">
+              <LabelJobInfo type={'category'} text={category} />
+            </div>
+          </div>
+          <div className="flex justify-start items-center gap-2 flex-wrap">
+            {tags.map((tag, index) => {
+              if (index < 6) return <LabelTag key={tag} text={tag} />;
+            })}
+          </div>
+        </div>
+        <JobSaveIcon
+          job_id={id.toString()}
+          category={category}
+          company_name={company_name}
+          search={title}
+        />
+      </button>
+    );
+  }
 }
--- src/components/custom/LabelJobInfo.tsx
+++ src/components/custom/LabelJobInfo.tsx
@@ -10,18 +10,32 @@ import React from 'react';
 interface LabelJobInfoProps {
   type: string;
   text: string;
+  big?: boolean;
 }
 
-export function LabelJobInfo({ type, text }: LabelJobInfoProps) {
-  const classIcon = 'text-[#fff]/50';
+export function LabelJobInfo({ type, text, big }: LabelJobInfoProps) {
+  const classIcon = `text-[#fff]/${big ? '100' : '50'} me-1`;
+  const bigSize = big ? 20 : 16;
   return (
     <p className="flex justify-center items-center">
-      {type == 'company' && <Building className={classIcon} height={16} />}
-      {type == 'location' && <MapPin className={classIcon} height={16} />}
-      {type == 'cash' && <Banknote className={classIcon} height={16} />}
-      {type == 'contract' && <FileText className={classIcon} height={16} />}
+      {type == 'company' && (
+        <Building className={classIcon} width={bigSize} height={bigSize} />
+      )}
+      {type == 'location' && (
+        <MapPin className={classIcon} width={bigSize} height={bigSize} />
+      )}
+      {type == 'cash' && (
+        <Banknote className={classIcon} width={bigSize} height={bigSize} />
+      )}
+      {type == 'contract' && (
+        <FileText className={classIcon} width={bigSize} height={bigSize} />
+      )}
       {type == 'category' && (
-        <TableProperties className={classIcon} height={16} />
+        <TableProperties
+          className={classIcon}
+          width={bigSize}
+          height={bigSize}
+        />
       )}
       <span>{text}</span>
     </p>
