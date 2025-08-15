b96eea207bb6217f95eb1d15a435daf3aff0b09a
Marcos Pedro <king.frontweb@outlook.com>
2025-08-15 14:37:23 -0300

    feat: dropdownmenu / page my job saves

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -4,6 +4,16 @@ import { createClient } from '@/src/lib/supabase/client';
 import { Button } from '@/src/components/ui/button';
 import { useRouter } from 'next/navigation';
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from './ui/dropdown-menu';
+import { APP_URL } from '../config';
+
 export function LogoutButton() {
   const router = useRouter();
 
@@ -14,11 +24,34 @@ export function LogoutButton() {
   };
 
   return (
-    <Button
-      onClick={logout}
-      className="bg-[#FE8A00] text-white font-bold text-lg hover:bg-[#CE7000]"
-    >
-      Logout
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger className="bg-[#FE8A00] text-white font-bold text-lg hover:bg-[#CE7000] px-3 py-1 rounded-lg">
+        Minha conta
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-gray-900">
+        <DropdownMenuItem
+          className="cursor-pointer hover:bg-gray-950"
+          onClick={() => {
+            router.push(`${APP_URL}/jobs/favorites`);
+          }}
+        >
+          Minhas vagas salvas
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="text-red-600 cursor-pointer hover:bg-gray-950"
+          onClick={logout}
+        >
+          Sair da conta
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
+  // return (
+  //   <Button
+  //     onClick={logout}
+  //     className="bg-[#FE8A00] text-white font-bold text-lg hover:bg-[#CE7000]"
+  //   >
+  //     Logout
+  //   </Button>
+  // );
 }
