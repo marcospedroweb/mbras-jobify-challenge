57fe0aa4c7fc6e13b23f3d1b4942b8bf95db9080
Marcos Pedro <king.frontweb@outlook.com>
2025-08-18 10:15:22 -0300

    fix: supabase with docker removed

--- supabase/migrations/20250818111308_create_jobs_table.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists jobs (
-  id uuid primary key default gen_random_uuid(),
-  created_at timestamp with time zone default now(),
-  company_name text not null,
-  title text not null,
-  category text not null,
-  job_id int4 not null
-);
